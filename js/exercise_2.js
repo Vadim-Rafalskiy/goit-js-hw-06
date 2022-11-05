21c81f4a2a173d6ebe9c456b6c1d3f1f93023824
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2022-11-05 23:06:24 +0200

    finish exercise_2

--- js/exercise_2.js
+++ js/exercise_2.js
@@ -1,5 +1,16 @@
 'use strict';
 
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
 const refs = {
   list: document.querySelector('#ingredients'),
 };
+
+const items = ingredients.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingredient;
+  itemEl.classList.add('item');
+  return itemEl;
+});
+
+refs.list.append(...items);

07399c28ee417e1e94fd8caee63a81406c7e330e
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2022-11-07 16:47:04 +0200

    finish exercise #7

--- js/exercise_7.js
+++ js/exercise_7.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const refs = {
+  input: document.querySelector('#font-size-control'),
+  text: document.querySelector('#text'),
+};
+
+const handlInput = e => {
+  refs.text.style.fontSize = `${e.target.value}px`;
+};
+
+refs.input.addEventListener('input', handlInput);
